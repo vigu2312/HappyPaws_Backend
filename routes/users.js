b4a7d4fecf08b6fb11886e7f7d5a41f5409dc2e3
Moni Shah <mn676765@dal.ca>
2020-07-24 10:34:52 -0300

    Update routes for forget password

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth')
-const { register, login, userDetails, logout, editProfile, forgetPassword } = require('../api/user');
+const { register, login, userDetails, logout, editProfile, forgetPassword, forgetPasswordMail } = require('../api/user');
 
 //User Model
 const User = require('../models/User');
@@ -10,6 +10,7 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/userDetails').get(auth, userDetails);
 router.route('/forgetPassword').put(auth, forgetPassword);
+router.route('/forgetPasswordMail').post(forgetPasswordMail);
 router.route('/editProfile').put(auth, editProfile);
 router.route('/logout').get(auth, logout);
 
