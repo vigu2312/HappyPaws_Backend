0af0e45d114504bc6a381de4b57dc93afb38db42
Moni Shah <mn676765@dal.ca>
2020-07-25 04:57:17 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
+/************
+ * Author: Moni Shah 
+ **********/
 const mongoose = require('mongoose');
 
+// DB configurations
 const connectDB = async () => {
     try{
         const connection = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useFindAndModify: false
         });
     console.log(`MongoDB connected: ${connection.connection.host}`);
     } catch (err) {
