36e6489a4649f1be693df2d32bbdafaaf2896867
Moni Shah <mn676765@dal.ca>
2020-07-25 04:57:48 -0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,8 @@
+/************
+ * Author: Moni Shah 
+ **********/
+
+ //Mongo URL
 module.exports = {
     mongoURI: 'mongodb+srv://moni:moni123@happypaws-db.oiovt.mongodb.net/HappyPaws?retryWrites=true&w=majority'
 }
